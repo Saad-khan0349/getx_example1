93d824ea8bbf8e95477e1b6ff21029aac2f6d904
Saad-Khan0349 <k.saad55551@gmail.com>
2025-11-05 12:01:34 -0800

    added 2nd example of Getx state management

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,26 +3,34 @@ import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:getx_clean_coding/counter_controller.dart';
+import 'package:getx_clean_coding/example_two.dart';
 
 class HomeScreen extends StatelessWidget {
-  final CounterController controller = Get.put(CounterController());
+  ExampleTwoController exampleTwoController = Get.put(ExampleTwoController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Home Screen')),
-      body: Center(
-        child: Obx(() {
-          print('rebuild');
-          return Text(
-            controller.counter.toString(),
-            style: TextStyle(fontSize: 55),
-          );
-        }),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          controller.incrementCounter();
-        },
+      body: Column(
+        children: [
+          Obx(
+            () => Container(
+              width: 200,
+              height: 200,
+              color: Colors.blue.withOpacity(
+                exampleTwoController.opacity.value,
+              ),
+            ),
+          ),
+          Obx(
+            () => Slider(
+              value: exampleTwoController.opacity.value,
+              onChanged: (value) {
+                exampleTwoController.setOpacity(value);
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
